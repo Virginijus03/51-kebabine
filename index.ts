6dbde3cb97828e930d36e30aa65e1e65732a299e
Virginijus03 <77634732+Virginijus03@users.noreply.github.com>
2021-10-17 12:42:26 +0300

    papildyta class, array ir t.t.

--- index.ts
+++ index.ts
@@ -1,25 +1,60 @@
+// https://www.typescriptlang.org/docs/handbook/2/objects.html
+interface ProduktasI {
+    pavadinimas: string;
+    svoris: number;
+    kaina: number;
+    barcode: number;
+}
+
 class Produktas {
+    public get barcode(): number {
+        return this._barcode;
+    }
+
     public readonly pavadinimas: string;
     public readonly svoris: number;
     public readonly kaina: number;
-    protected readonly barcode: number;
+    private readonly _barcode: number;
 
     public constructor(pavadinimas: string,
         svoris: number,
-        kaina: number) {
+        kaina: number,
+        barcode?: number) {
 
         this.kaina = kaina;
         this.svoris = svoris;
         this.pavadinimas = pavadinimas;
 
-        this.barcode = 100000 + Math.round(Math.random() * 10000);
+        this._barcode = barcode || 100000 + Math.round(Math.random() * 10000);
     }
 
-    public spausdintiDuomenis(): void {
-        console.log(`Produktas: ${this.pavadinimas}`);
-        console.log(`Barkodas: ${this.barcode}`);
-        console.log(`Svoris: ${this.svoris} g.`);
-        console.log(`Kaina: ${this.kaina} eur.`);
+    public spausdintiDuomenis(element?: HTMLElement): void {
+        if (element) {
+            element.innerHTML += `
+                <div class="card">
+                    <div class="controls">
+                        <img onclick="istrintiProdukta(${this._barcode})" class="icon delete" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png">
+                        <img onclick="kopijuotiProdukta(${this._barcode})" class="icon copy" src="https://cdn-icons-png.flaticon.com/512/54/54702.png">
+                    </div>
+                
+                    <h2>${this.pavadinimas}</h2>
+                    
+                    <ul>
+                        <li>Barkodas: <b>${this._barcode}</b></li>
+                        <li>Svoris: <b>${this.svoris} g.</b></li>
+                        <li>Kaina: <b>${this.kaina} eur.</b></li>
+                    </ul>
+                </div>`;
+        }
+    }
+
+    public toJSON(): any {
+        return {
+            kaina: this.kaina,
+            svoris: this.svoris,
+            pavadinimas: this.pavadinimas,
+            barcode: this._barcode
+        };
     }
 }
 
@@ -28,30 +63,6 @@ enum BulvytesTipas {
     Lazdeles = "lzdl",
     Laiveliai = "lvl",
     Puseles = "psls"
-}
-
-// Public - metodą arba atributą gali pasiekti bet kas
-// Private - metodą arba atributą gali pasiekti tik pati klasė
-// Protected - metodą arba atributą gali pasiekti tik pati arba
-// vaikinė klasė
-// Pvz.:
-class A {
-    public x: number;
-    private y: number;
-    protected z: number;
-}
-
-const a = new A();
-// Galime pasiekti tik x atributą, nes jis vienintelis yra public.
-a.x;
-
-class B extends A {
-    public metodas(): void {
-        // Galime pasiekti tik x ir z atributu, nes jie nėra private
-        // yra public.
-        this.z;
-        this.x;
-    }
 }
 
 // Paveldėjimas
@@ -125,17 +136,21 @@ class Kebabas extends Produktas {
     }
 }
 
+const PRODUCTS_LOCAL_STORAGE_KEY = "products";
+
 const UI = {
     // https://stackoverflow.com/questions/13204759/typescript-or-javascript-type-casting
     nameInput: document.getElementById("produktoPavadinimas") as HTMLInputElement,
     priceInput: document.getElementById("produktoKaina") as HTMLInputElement,
     weightInput: document.getElementById("produktoSvoris") as HTMLInputElement,
     addButton: document.getElementById("pridetiProdukta") as HTMLButtonElement,
+    // https://www.typescriptlang.org/docs/handbook/2/generics.html
+    menuContainer: document.querySelector(".menu") as HTMLDivElement
 }
 
-const produktai: Produktas[] = [];
+let produktai: Produktas[] = [];
 
-UI.addButton.addEventListener("click", (e) => {
+UI.addButton.addEventListener("click", () => {
     const pavadinimas = UI.nameInput.value;
     const svoris = Number(UI.weightInput.value);
     const kaina = Number(UI.priceInput.value);
@@ -144,5 +159,85 @@ UI.addButton.addEventListener("click", (e) => {
 
     produktai.push(naujasProduktas);
 
-    console.log(produktai);
+    atvaizduotiProduktus();
+    saveProducts();
 });
+
+function atvaizduotiProduktus(): void {
+    UI.menuContainer.innerHTML = "";
+
+    for (const produktas of produktai) {
+        produktas.spausdintiDuomenis(UI.menuContainer);
+    }
+}
+
+function kopijuotiProdukta(barcode: number): void {
+    console.log("Kopijuoti produktą!");
+
+    const produktas = produktai.find((produktas) => produktas.barcode === barcode);
+
+    if (!produktas)
+        throw new Error("Produktas nerastas!");
+
+    const naujasProduktas = new Produktas(produktas.pavadinimas, produktas.svoris, produktas.kaina);
+    produktai.push(naujasProduktas);
+    naujasProduktas.spausdintiDuomenis(UI.menuContainer);
+    saveProducts();
+}
+
+function istrintiProdukta(barcode: number): void {
+    console.log("Trinti produktą...", barcode);
+
+    // const produktroIndeksas = produktai.findIndex((produktas) => {
+    //     return produktas.barcode === barcode;
+    // });
+    //
+    // if (produktroIndeksas === -1)
+    //     throw new Error("Product not found");
+    //
+    // produktai.splice(produktroIndeksas, 1);
+
+    produktai = produktai.filter((produktas) => produktas.barcode !== barcode);
+
+    atvaizduotiProduktus();
+    saveProducts();
+}
+
+function loadProducts(): void {
+    const p = window.localStorage.getItem(PRODUCTS_LOCAL_STORAGE_KEY);    // "[{"kaina":123,"svoris":33,"pavadinimas":"Lego","_barcode":102279}]"
+
+    if (!p) {
+        return;
+    }
+
+    // !"" - true
+    // !"{}" - false
+
+    const produktaiBeMetodu: ProduktasI[] = JSON.parse(p);
+
+    for (const produktas of produktaiBeMetodu) {
+        const naujasProduktas = new Produktas(
+            produktas.pavadinimas,
+            produktas.svoris,
+            produktas.kaina,
+            produktas.barcode);
+
+        produktai.push(naujasProduktas);
+    }
+
+    const barkodai = produktai.map((produktas) => {
+        return produktas.barcode;
+    });
+
+    console.log(barkodai);
+
+    atvaizduotiProduktus();
+}
+
+function saveProducts(): void {
+    const produktaiString = JSON.stringify(produktai);
+
+    window.localStorage.setItem(PRODUCTS_LOCAL_STORAGE_KEY, produktaiString);
+}
+
+loadProducts();
